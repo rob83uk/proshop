c4828e2196d4ec6eb27658647da3f8ccf4e25036
Rob Crowther <robcrowther@hotmail.com>
2022-03-18 21:13:43 +0000

    basic server set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Welcome to Proshop</h1>
-    </div>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
